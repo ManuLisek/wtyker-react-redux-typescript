e1780125a7314c0330b675083930383381dcea1e
ManuLisek <miclisowiec@gmail.com>
2025-04-02 12:30:11 +0200

    Create an axiosConfig.ts

---
+++ src/api/axiosConfig.ts
@@ -0,0 +1,48 @@
+import axios from 'axios';
+import { ProductType } from '../types/types';
+
+interface GetProductResponse {
+  product: ProductType;
+}
+
+const instance = axios.create({
+  baseURL: '/',
+});
+
+const url = '/products.json';
+
+export const getProduct = (id: number) =>
+  instance({
+    method: 'GET',
+    url: url,
+    transformResponse: [
+      (response: string): GetProductResponse => {
+        const products: ProductType[] = JSON.parse(response);
+
+        const foundProduct = products.find((product) => product.id === id);
+
+        if (!foundProduct) {
+          throw new Error(`Nie znaleziono takiego produktu.`);
+        }
+
+        const transformedProduct: ProductType = {
+          id: foundProduct.id,
+          title: foundProduct.title,
+          price: foundProduct.price,
+          brand: foundProduct.brand,
+          description: foundProduct.description,
+          tags: foundProduct.tags,
+          image1: foundProduct.image1,
+          image2: foundProduct.image2,
+        };
+
+        return {
+          product: transformedProduct,
+        };
+      },
+    ],
+  });
+
+export default {
+  getProduct,
+};
