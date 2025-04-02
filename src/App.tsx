dcc45edb87b74a15f088dee6ff08d1dfe0adbd29
ManuLisek <miclisowiec@gmail.com>
2025-04-02 21:32:43 +0200

    Vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './Views/Home/Home';
 import ProductsList from './Views/ProductsList/ProductsListContainer';
 import Product from './Views/Product/ProductContainer';
@@ -9,7 +9,7 @@ import ScrollToTop from './utilities/ScrollToTop';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <ScrollToTop>
         <MainLayout>
           <Routes>
