83918e572fcb3cf529a2df1bb583e847b62879fb
ManuLisek <miclisowiec@gmail.com>
2025-04-02 21:11:40 +0200

    Revert HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './Views/Home/Home';
 import ProductsList from './Views/ProductsList/ProductsListContainer';
 import Product from './Views/Product/ProductContainer';
